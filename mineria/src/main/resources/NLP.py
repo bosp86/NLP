38b6b7d32c437dc8690ab61ea3931a9f09d62c69
V622980 <V622980@IPE00156L152085.emea.dsmain.com>
2018-12-05 15:08:32 -0500

    Added functions

--- mineria/src/main/resources/NLP.py
+++ mineria/src/main/resources/NLP.py
@@ -2,7 +2,9 @@ import spacy
 import pandas as pd
 import numpy as np
 import nltk
-import NLPUtils
+
+# library to normalize text
+import normalization
 # use once?
 #nltk.download('stopwords')
 from nltk.tokenize.toktok import ToktokTokenizer
@@ -15,12 +17,12 @@ print ("BEGINING")
 # uncomment if it is the first time 
 #nltk.download('stopwords')
 news_df =  pd.read_csv('movieDescriptionDataSet.tsv', sep='\t')
-print (news_df.head(10))
+#print (news_df.head(10))
 
-print (news_df.Genre.value_counts())
+#print (news_df.Genre.value_counts())
 
 
-news_df['clean_text'] = NLPUtils.normalize_corpus(news_df['Description'])
+news_df['clean_text'] = normalization.normalize_corpus(news_df['Description'])
 norm_corpus = list(news_df['clean_text'])
 
 print (news_df.iloc[1][['Description', 'clean_text']].to_dict())
